2d3662982da90cf6d925814e8b3831cd3dafde88
Denise Dearman <denisedearman@Patricks-MacBook-Pro.local>
2017-06-23 11:05:18 -0400

    created a fake list_regions method. created a welcome method. created a menu method that can exit

--- lib/wiser_wine/cli.rb
+++ lib/wiser_wine/cli.rb
@@ -1,6 +1,32 @@
 class WiserWine::CLI
-  
+
   def call
+    menu
+  end
+
+  def welcome
     puts "Welcome to Wiser Wine"
   end
+
+  def list_regions
+    puts "
+    1. USA
+    2. France
+    3. Germany"
+  end
+
+  def menu
+    welcome
+    puts "Enter the number for a wine region from the list provided to get more info or exit to quit."
+    list_regions
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+    end
+
+  end
+
+
+
+
 end
