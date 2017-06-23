4314ca568bd6b71b3175caf28abf859a02143233
Denise Dearman <denisedearman@Patricks-MacBook-Pro.local>
2017-06-23 11:08:44 -0400

    Added menu options for the list

--- lib/wiser_wine/cli.rb
+++ lib/wiser_wine/cli.rb
@@ -22,6 +22,18 @@ class WiserWine::CLI
     input = nil
     while input != "exit"
       input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on USA"
+      when "2"
+        puts "More info on France"
+      when "3"
+        puts "More info on Germany"
+      when "exit"
+        puts "Goodbye friend!"
+      else
+        puts "I didn't understand what you wanted."
+      end
     end
 
   end
